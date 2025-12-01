e6f3fed6bbbfbf1d76c338952c5bf613454cb87e
cpoli87 <crispolitowski@gmail.com>
2025-12-01 04:08:17 +0000

    Deploying to gh-pages from @ cpoli87/cpoli87.github.io@69aea0fd1b2f337f74369542030c7c6a71107e60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/2025-11-30-spatial-hashing/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
